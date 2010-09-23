7c1c59ad365e519700ba1bcf38dae1f51cce7e4a
Tadeusz Sośnierz <tadzikes@gmail.com>
2010-09-23 13:50:16 +0000

    Bugfix in Module::Test

--- lib/Module/Test.pm
+++ lib/Module/Test.pm
@@ -3,12 +3,12 @@ module Module::Test;
 our sub test(Str $dir = '.', Str $binary = 'perl6', :$v) {
     if $*VM<config><osname> ne 'MSWin32'
     && "$dir/Makefile".IO ~~ :f {
+        return if run 'make -q test';
         my $cwd = cwd;
         chdir $dir;
         run 'make test' and die "'make test' failed";
         chdir $cwd;
-    }
-    if "$dir/t".IO ~~ :d {
+    } elsif "$dir/t".IO ~~ :d {
         my $x = $v ?? '-v' !! '-Q';
         my $command = "PERL6LIB=$dir/lib prove $x -e $binary -r $dir/t/";
         run $command and die 'Testing failed';
